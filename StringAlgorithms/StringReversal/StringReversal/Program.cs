656b3fc54ae7780021a9a5fa2e372822d3d36c2c
Luidy Naves <luidymorais@gmail.com>
2021-12-03 20:55:45 -0500

    String reversal

---
+++ StringAlgorithms/StringReversal/StringReversal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StringReversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse("Hello World!"));
+
+            Console.ReadLine();
+        }
+
+        static string Reverse(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return str;
+
+            string newString = string.Empty;
+
+            for (int index = str.Length - 1; index > -1; index--)
+            {
+                newString += str[index];
+            }
+
+            return newString;
+        }
+    }
+}
